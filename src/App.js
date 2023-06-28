745f05b2934efe31567bd05c88f97270345feb2f
james kim <hun935701@gmail.com>
2023-06-28 18:04:22 -0500

    added data binding and state for the varibale that will change often. also updated UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  let post = 'title1';
+  // destructring title will hod 'database design recommendation'
+  let [title,b] = useState('database design recommendation')
+  //state used for re-rendering when the data changes often
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{fontSize:'18'}}>BLOG</h4>
+      </div>
+      <div className="list">
+        <h4>{title}</h4>
+        <p>posted 06/28/2023</p>
+      </div>
     </div>
   );
 }
