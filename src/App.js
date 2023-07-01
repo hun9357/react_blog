a18f0fee5a6e201baa70b65a4b893bcff3a35c36
james kim <hun935701@gmail.com>
2023-07-01 13:38:32 -0500

    added like onclick state management

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+/*eslint-disable*/ 
+
 import './App.css';
 import {useState} from 'react'
+
 
 function App() {
 
   let post = 'title1';
   // destructring title will hod 'database design recommendation'
-  let [title,b] = useState('database design recommendation')
+  let [title,b] = useState(['Database design recommendation','what is coding','Python for beginners'])
+  let [likes,likesChange] = useState(0)
   //state used for re-rendering when the data changes often
-
 
   return (
     <div className="App">
@@ -16,7 +18,18 @@ function App() {
         <h4 style={{fontSize:'18'}}>BLOG</h4>
       </div>
       <div className="list">
-        <h4>{title}</h4>
+        <h4>{title[0]} <span onClick= {()=>{likesChange(likes+1)}}>👍</span> {likes} </h4>
+        <p>posted 06/28/2023</p>
+      </div>
+
+
+
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>posted 06/28/2023</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
         <p>posted 06/28/2023</p>
       </div>
     </div>
